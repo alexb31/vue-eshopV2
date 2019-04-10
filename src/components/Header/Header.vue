7a4639f7ad60f13b586cb70385bbeb95ebd14036
alexb31 <alexandrebornstein@gmail.com>
2019-04-10 23:26:03 +0200

    Added Header Navbar

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <v-toolbar app>
+      <v-spacer class="hidden-md-and-up"></v-spacer>
+      <v-btn flat>{{appTitle}}</v-btn>
+      <v-spacer class="hidden-sm-and-down"></v-spacer>
+      <search></search>
+      <v-btn color="success" class="hidden-sm-and-down">Cart</v-btn>
+      <v-btn flat class="hidden-sm-and-down">Login</v-btn>
+      <v-btn flat class="hidden-sm-and-down">Register</v-btn>
+    </v-toolbar>
+  </span>
+</template>
+
+<script>
+import Search from "../Search/Search";
+
+export default {
+  components: {
+    Search
+  },
+  data() {
+    return {
+      appTitle: "Vue Shop"
+    };
+  }
+};
+</script>
---
+++ src/components/Search/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-text-field class="ml-2" label="Search..." append-icon="clear" hide-details></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search"
+};
+</script>
