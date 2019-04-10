9e6f1b4f2ab7d0da0fa5ee1973aa9cd1792f4c46
alexb31 <alexandrebornstein@gmail.com>
2019-04-11 00:16:19 +0200

    Search Filter, Wip

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <v-text-field class="ml-2" label="Search..." append-icon="clear" hide-details></v-text-field>
+    <v-text-field
+      v-model="value"
+      @keyup="search(value)"
+      class="ml-2"
+      label="Search..."
+      append-icon="clear"
+      hide-details
+    ></v-text-field>
   </div>
 </template>
 
 <script>
 export default {
-  name: "search"
+  name: "search",
+  data() {
+    return {
+      value: ""
+    };
+  },
+  methods: {
+    search(value) {
+      if (value.length > 0) {
+        console.log("True");
+      }
+    }
+  }
 };
 </script>
